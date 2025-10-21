9e6286e65780c1b4f4fc094926c364aed1b54e00
Erik Santos <erikluan@alumni.usp.br>
2025-10-21 16:22:32 -0300

    ajustes base silver

--- uploads_servidor/upload_base_silver.R
+++ uploads_servidor/upload_base_silver.R
@@ -4,31 +4,10 @@ source('auxiliares/municipios_sicredi.R')
 # Base Silver
 df_08 <- readxl::read_excel('dados/r20250819_silver_associados_pj.xlsx') %>% distinct()
 
-dbWriteTable(con, 'sicredi_silver_o', df_08, overwrite = TRUE)
+#dbWriteTable(con, 'sicredi_silver_o', df_08, overwrite = TRUE)
 
 
-# Base Silver tratada
-
-df_08_final <- df_08 |> 
-  group_by(municipio_y) |> 
-  mutate(freq_mu = n()) |> 
-  ungroup() |> 
-  filter(
-    freq_mu >= 10,
-    !is.na(municipio_y),
-    !is.na(estado_y),
-    estado_y %in% c("TO", "MS", "BA"),
-    estado_x %in% c("TO", "MS", "BA"),
-    (`F`|`M` != 0),
-    (`Mais de 60` + `Menos de 50` + `Menos de 60`  > 0),
-    status_associado=='ATIVO'
-  ) |>
-  group_by(municipio_x) |> 
-  mutate(freq_mu = n()) |> 
-  ungroup() |> 
-  filter(
-    freq_mu >= 10
-  ) |> 
+df_08 <- df_08 |> 
   mutate(
     sexo_final = case_when(
       `F` == 50 ~ "Ambos",
@@ -38,10 +17,17 @@ df_08_final <- df_08 |>
     ),
     
     predominancia = case_when(
-      `Mais de 60` >= 50 & `F`==50 ~ "Ambiguo - Idosos",
-      `Mais de 60` >= 50 & `F`>=51 ~ "Mulheres idosas",
-      `Mais de 60` >= 50 & `F`<=51 ~ "Homens idosos",
+      `Mais de 60` >= 50 & `F`==50 ~ "Ambiguo - Silver",
+      `Mais de 60` >= 50 & `F`>=51 ~ "Mulheres Silver",
+      `Mais de 60` >= 50 & `F`<=51 ~ "Homens Silver",
       TRUE ~ "Classificar"
+    ),
+    
+    idade_classificada = case_when(
+      `Mais de 60` >= 50 ~ "Silver",
+      `Menos de 50` >= 50 ~ "Não-Silver",
+      `Menos de 60` >= 50  ~ "Pré-Silver",
+      TRUE ~ "Indefinido"
     ),
     
     # Converter para data/hora corretamente
@@ -49,4 +35,29 @@ df_08_final <- df_08 |>
     tempo_relacionamento = interval(assoc_desde, ymd("2025-05-31")) / months(1)
   )
 
+# Base Silver tratada
 
+df_08_final <- df_08 |> 
+  # group_by(municipio_y) |> 
+  # mutate(freq_mu = n()) |> 
+  # ungroup() |> 
+  filter(
+    #  freq_mu >= 10,
+    !is.na(municipio_y),
+    !is.na(estado_y),
+    estado_y %in% c("TO", "MS", "BA"),
+    estado_x %in% c("TO", "MS", "BA"),
+    (`F`|`M` != 0),
+    (`Mais de 60` + `Menos de 50` + `Menos de 60`  > 0),
+    status_associado=='ATIVO'
+  ) 
+#group_by(municipio_x) |> 
+#mutate(freq_mu = n()) |> 
+#ungroup() |> 
+#filter(
+#  freq_mu >= 10
+#) |> 
+
+
+
+a <- df_08 |> filter(status_associado=='ATIVO')|>  select(`Mais de 60`, `Menos de 50`, `Menos de 60`, `F`, `M`, sexo_final, predominancia, idada_classificada)
